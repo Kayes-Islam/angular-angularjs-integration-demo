12dd6ca94805e4a74c9552e3e77ab892514efdf4
Kayes Islam <kayes.islam@oceanhealthsystems.com>
2018-11-05 17:47:00 +1030

    first commit

---
+++ Recipe.Central.Mvc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Recipe.Central.Startup))]
+namespace Recipe.Central
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
